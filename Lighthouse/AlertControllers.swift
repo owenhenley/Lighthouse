5d0e8ef6822d65bb9220acd199466cd2bffe994c
Levi Linchenko <levilinchenko@Levis-MacBook-Pro.local>
2018-10-08 17:58:06 -0600

    Add login functionality

---
+++ Lighthouse/AlertControllers.swift
@@ -0,0 +1,14 @@
+//
+//  AlertControllers.swift
+//  Lighthouse
+//
+//  Created by Levi Linchenko on 08/10/2018.
+//  Copyright © 2018 Lighthouse. All rights reserved.
+//
+
+import UIKit
+
+//extension UIViewController {
+//    let locationAlertController = UIAlertController(title: "Lighthouse needs to use your location", message: <#T##String?#>, preferredStyle: <#T##UIAlertController.Style#>)
+//}
+
--- Lighthouse/UserController.swift
+++ Lighthouse/UserController.swift
@@ -11,30 +11,64 @@ import Firebase
 
 class UserController {
     
+    private init() {}
+    
+    static let shared = UserController()
+    
     var users: [User] = []
    
     let db = Firestore.firestore()
+
     
-    func createUser(userID: String, username: String, email: String, currentLocation: Location){
-        db.collection(USER).addDocument(data: [
-            USERID : userID,
-            USERNAME : username
-        ]) { (error) in
+    func logInUser(email: String, password: String){
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
             if let error = error {
                 print ("💩💩 error in file \(#file), function \(#function), \(error),\(error.localizedDescription)💩💩")
-                return
-            } else {
-                let user = User(userID: userID, currentLocation: currentLocation, username: username, email: email)
-                self.users.append(user)
+                //FIXME: presentAlertController
             }
-            
-            
-            
-            
-            
         }
     }
     
-  
+    func createUser(username: String, email: String, password: String, currentLocation: Location){
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                print ("💩💩 error in file \(#file), function \(#function), \(error),\(error.localizedDescription)💩💩")
+            }
+            if let result = result {
+                self.db.collection(USER).addDocument(data: [
+                    USERID : result.user.uid,
+                    USERNAME : username
+                ]) { (error) in
+                    if let error = error {
+                        print ("💩💩 error in file \(#file), function \(#function), \(error),\(error.localizedDescription)💩💩")
+                        return
+                    } else {
+                        let user = User(userID: result.user.uid, currentLocation: currentLocation, username: username, email: email)
+                        self.users.append(user)
+                    }
+                }
+            }
+        }
+    }
+    
+    func fetchUsers(){
+        
+    }
+    
+    func updateUser(userID: String, username: String?, email: String?, profileImageURL: URL?){
+        
+    }
+    
+    func togglePrivacy(userID: String, isActive: Bool){
+        
+    }
+    
+    func deleteUser(userID: String){
+        }
+    
+    func addFriend(friendUserID: String){
+        
+    }
+    
     
 }
