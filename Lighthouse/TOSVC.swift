57a56bd8779a787f1a1eea921ad5f5f59065e2a4
Owen Henley <owenhenley@me.com>
2018-10-12 10:34:48 -0600

    Storyboards added, navigation started, Commit for Levi to catch up

---
+++ Lighthouse/TOSVC.swift
@@ -0,0 +1,30 @@
+//
+//  TOSVC.swift
+//  Lighthouse
+//
+//  Created by Owen Henley on 10/12/18.
+//  Copyright © 2018 Lighthouse. All rights reserved.
+//
+
+import UIKit
+
+class TOSVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
