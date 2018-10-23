c0b734e15f96b2cc8e30bff25fcbbb08b300f437
Owen Henley <owenhenley@me.com>
2018-10-22 20:11:06 -0600

    Navigation to AddVC from Friends Tab ++++
    Can navigate from friends tab to Add VC, Invite button code implemented.

--- Lighthouse/AddFriendsVC.swift
+++ Lighthouse/AddFriendsVC.swift
@@ -10,21 +10,59 @@ import UIKit
 
 class AddFriendsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backTapped(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
     }
-    */
+    
+    @IBAction func inviteFriendsTapped(_ sender: UIButton) {
+        
+        let message = "Hey! I got this new free app 'Lighthouse', lets hang out!"
+        let appStoreLink = "AppStore link coming soon."
+        
+        let messageToShare = [message, appStoreLink]
+        let shareAlert = UIActivityViewController(activityItems: messageToShare, applicationActivities: nil)
+        
+        shareAlert.popoverPresentationController?.sourceView = sender
+        present(shareAlert, animated: true, completion: nil)
+    }
+}
 
+extension AddFriendsVC: UITableViewDataSource, UITableViewDelegate {
+    
+    
+    //MARK: UITableViewDataSource
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return FriendController.shared.pendingReuests.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "<#CellID#>", for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            // Delete the row from the table view
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    
 }
