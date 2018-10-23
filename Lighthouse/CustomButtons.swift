786b2528a6b39e4ac6c2e23c9cf2e1e5c6873687
Owen Henley <owenhenley@me.com>
2018-10-23 08:14:21 -0600

    Assets Re-added, Profile constrained and implimented

---
+++ Lighthouse/CustomButtons.swift
@@ -0,0 +1,19 @@
+//
+//  CustomButtons.swift
+//  Lighthouse
+//
+//  Created by Owen Henley on 10/23/18.
+//  Copyright © 2018 Lighthouse. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func orangeButton() {
+    }
+
+    func presentation() {
+        
+    }
+}
